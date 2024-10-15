3127c7bdaa7b4371787742e73f6b72249ffd60af
RitaViegas <ana.rita.viegas77@gmail.com>
2024-10-15 11:38:26 +0100

    RitaViegas --- project Spotify

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
@@ -12,15 +13,47 @@ client_id = os.environ.get("CLIENT_ID")
 client_secret = os.environ.get("CLIENT_SECRET")
 
 auth_manager = SpotifyClientCredentials(client_id = client_id, client_secret = client_secret)
-con = spotipy.Spotify(auth_manager=auth_manager)
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
-artist_id = "0PCCGZ0wGLizHt2KZ7hhA2"
 artist = 'spotify:artist:0PCCGZ0wGLizHt2KZ7hhA2'
 
+results = sp.artist_top_tracks(artist, country='US')
 
-response = sp.artist_top_tracks("0PCCGZ0wGLizHt2KZ7hhA2")
-if response:
-  # We keep the "tracks" object of the answer
-  tracks = response["tracks"]
-  # We select, for each song, the data we are interested in and discard the rest
-  tracks = [{k: (v/(1000*60))%60 if k == "duration_ms" else v for k, v in track.items() if k in ["name", "popularity", "duration_ms"]} for track in tracks]
+
+if results:
+  tracks = results["tracks"]
+  #{k: v for k, v in track.items() if k in ["name", "popularity", "duration_ms"]}
+  tracks = [
+    {key: (value/(1000*60)) if key == "duration_ms" else value for key, value in track.items() if key in ["name", "popularity", "duration_ms"]} for track in tracks
+    ]
+
+
+# Convert the list of dictionaries to a Pandas DataFrame
+tracks_df = pd.DataFrame(tracks)
+
+# Sort by popularity in ascending order
+tracks_df.sort_values(by= 'popularity', ascending=True)  
+
+plt.figure(figsize=(8,6))
+sns.scatterplot(data=tracks_df, x='duration_ms', y='popularity', color='purple' )
+
+
+plt.title('Relationship between duration and popularity of songs')
+plt.xlabel('Duration (minutes)')
+plt.ylabel('Popularity')
+
+
+plt.show()
+
+# Calculate the correlation between duration and popularity
+correlation = tracks_df['popularity'].corr(tracks_df['duration_ms'])
+print(f"Correlación entre popularidad y duración: {correlation:.2f}")
+
+
+
+# Statistical analysis:
+#  A correlation value of 0.02 between song duration and popularity indicates
+#  that there is no significant linear relationship between the two variables.
+#  This means that the length of a song has little to no impact on its popularity.
+
+
